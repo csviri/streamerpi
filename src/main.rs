48720e48f007f391f83f071237bac6c195b57dae
csviri <csviri@gmail.com>
2020-10-10 23:28:30 +0200

    listing files

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,41 @@
-use warp::Filter;
 use std::fs;
-use std::error::Error;
-use std::fs::ReadDir;
 use std::result::Result;
+
+use warp::Filter;
 
 #[tokio::main]
 async fn main() {
     // GET /hello/warp => 200 OK with body "Hello, warp!"
     let file_list = warp::path!("files")
-        .map(|| {
-            format!("File list: {}", file_list_html("."))
-        });
+        .map(|| warp::reply::html(file_list_html(".")));
 
-
+    println!("Starting server!");
     warp::serve(file_list)
         .run(([127, 0, 0, 1], 3030))
         .await;
 }
 
-fn file_list_html(dir: &str) -> String {
+async fn file_list_html(dir: &str) -> String {
     let mut result: String = String::new();
     result.push_str("<html><body><ul>");
     let dir = fs::read_dir(dir);
     match dir {
-        // todo message
-        Result::Err(err) => { result.push_str("ERROR!!")}
+        Result::Err(_) => { result.push_str("ERROR!! : ") }
         Result::Ok(read_dir) => {
             for entry in read_dir {
-                // I'm here
-                result.push_str("<li>{}</li>",)
+                match entry {
+                    Ok(dir_entry) => {
+                        let file = dir_entry.file_name();
+                        match file.to_str() {
+                            Some(file) => { result.push_str(format!("<li>{}</li>", file).as_str()) }
+                            None => {}
+                        }
+                    }
+                    Err(err) => eprintln!("Error: {}", err)
+                }
             }
         }
     }
-
     result.push_str("</ul></body></html>");
     return result;
 }
