229850f268b1b6f971df27701751cedd55b8f543
csviri <csviri@gmail.com>
2020-10-11 00:08:34 +0200

    range simple

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,31 @@ use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    // GET /hello/warp => 200 OK with body "Hello, warp!"
-    let file_list = warp::path!("files")
-        .map(|| warp::reply::html(file_list_html(".")));
+    const DIR: &str = ".";
 
-    println!("Starting server!");
-    warp::serve(file_list)
+    let file_list = warp::path!("files")
+        .map(|| warp::reply::html(list_files_to_html(DIR)));
+
+    let stream = warp::get()
+        .and(warp::path("stream"))
+        .and(warp::path::param::<String>())
+        .and(warp::header::optional::<String>("range"))
+        .map(|name: String, range: Option<String>| {
+            let range_val = match range {
+                Some(range) => { range }
+                None => { "Not present.".to_string() }
+            };
+            format!("Name: {}, range: {}", name, range_val)
+        });
+
+    println!("Server starting!");
+    warp::serve(file_list.or(stream))
+
         .run(([127, 0, 0, 1], 3030))
         .await;
 }
 
-async fn file_list_html(dir: &str) -> String {
+fn list_files_to_html(dir: &str) -> String {
     let mut result: String = String::new();
     result.push_str("<html><body><ul>");
     let dir = fs::read_dir(dir);
