0f38d883295a6b5c25664be5c4be63e5d02070e8
csviri <csviri@gmail.com>
2020-10-13 22:29:52 +0200

    range cosmetics

--- src/stream/range.rs
+++ src/stream/range.rs
@@ -3,23 +3,28 @@ pub struct Range {
     pub end: Option<u64>,
 }
 
-pub fn parse_range(range_header: String) -> Result<Range, String> {
-    let interval = range_header.replace("bytes=", "");
-    let divider = interval.find("-");
+impl Range {
+    pub fn parse_range(range_header: String) -> Result<Range, String> {
+        let interval = range_header.replace("bytes=", "");
+        let divider = interval.find("-");
 
-    return match divider {
-        Some(pos) => {
-            let start = interval[0..pos].parse::<u64>().unwrap();
-            let end =
-                if interval.chars().count() == pos + 1 {
-                    None
-                } else {
-                    Some(interval[pos+1..interval.len()].parse::<u64>().unwrap())
-                };
-            Ok(Range { start, end })
-        }
-        None => {
-            Err(format!("Invalid range header: {}", range_header))
-        }
-    };
+        return match divider {
+            Some(pos) => {
+                //todo this can kill the server, improve
+                let start = interval[0..pos].parse::<u64>().unwrap();
+                let end =
+                    if interval.chars().count() == pos + 1 {
+                        None
+                    } else {
+                        //todo this can kill the server, improve
+                        Some(interval[pos+1..interval.len()].parse::<u64>().unwrap())
+                    };
+                Ok(Range { start, end })
+            }
+            None => {
+                Err(format!("Invalid range header: {}", range_header))
+            }
+        };
+    }
 }
+
