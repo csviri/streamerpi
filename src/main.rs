3e1740bdf357e1b95b70fbfd41201b6badd90e53
csviri <csviri@gmail.com>
2020-10-14 21:35:17 +0200

    progress, modules

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
-use std::{env, fs};
-use std::fs::{DirEntry, File};
-use std::io::Read;
+use std::{env};
 use std::path::PathBuf;
-use std::result::Result;
-
-use warp::{Filter};
-use streampi::stream::video::VideoStream;
-
-static VIDEO_PAGE_TEMPLATE: &'static str = include_str!("video.html");
+use warp::Filter;
+use streampi::stream::range::Range;
+use streampi::stream::video::{read_file_range_to_video_stream};
+use streampi::browse::dir::{list_files_to_html, video_html};
+use urlencoding::decode;
 
 // todo reading file as async?
+
+// todo this goes away with real streaming
+const MAX_STREAM_RESPONSE_SIZE: u64 = 2621440;
+
 
 #[tokio::main]
 async fn main() {
@@ -22,12 +23,17 @@ async fn main() {
     println!("Root dir: {}", root_dir);
 
     let file_list = warp::path::end()
-        .map(move || warp::reply::html(list_files_to_html(&root_clone)));
+        .map(move || warp::reply::html(list_files_to_html(&root_clone, "")));
 
     let dir_view = warp::get()
         .and(warp::path("dir"))
         .and(warp::path::param::<String>())
-        .map(move |dir: String| warp::reply::html(list_files_to_html(format!("{}/{}", root_clone2, dir).as_str())));
+        .map(move |dir: String| {
+            // todo error
+            let decoded_dir = decode(dir.as_str()).unwrap();
+            let html = list_files_to_html(root_clone2.as_str(), decoded_dir.as_str());
+            warp::reply::html(html)
+        });
 
     let video_page = warp::get()
         .and(warp::path("video"))
@@ -41,11 +47,22 @@ async fn main() {
         .and(warp::path::param::<String>())
         .and(warp::header::optional::<String>("range"))
         // .and_then(get_file_bytes);
-        .map(move |name: String, range: Option<String>| {
+        .map(move |path: String, range: Option<String>| {
             match range {
                 Some(range) => {
-                    let bytes = read_file_range(&root_dir, name, range);
-                    return VideoStream { bytes };
+                    // todo error handling
+                    let decoded_path = decode(path.as_str()).unwrap();
+                    let mut path = PathBuf::new();
+                    path.push(&root_dir);
+                    path.push(decoded_path);
+                    let range_result = Range::parse_range(range);
+                    match range_result {
+                        Ok(range) => {
+                            return read_file_range_to_video_stream(path, range, MAX_STREAM_RESPONSE_SIZE);
+                        }
+                        // todo error handling
+                        Err(err) => { panic!(err) }
+                    }
                 }
                 None => { panic!("TODO") }
             };
@@ -55,66 +72,4 @@ async fn main() {
     warp::serve(file_list.or(stream).or(video_page).or(dir_view))
         .run(([127, 0, 0, 1], 8080)) // todo port as param
         .await;
-}
-
-fn read_file_range(root_dir: &str, file_name: String, _range: String) -> Vec<u8> {
-    // todo respect range header
-    let mut path = PathBuf::new();
-    path.push(root_dir);
-    path.push(file_name);
-
-
-    let mut f = File::open(&path).expect("no file found");
-    let metadata = fs::metadata(&path).expect("unable to read metadata");
-    let mut buffer = vec![0; metadata.len() as usize];
-    f.read(&mut buffer).expect("buffer overflow");
-    buffer
-}
-
-fn video_html(file_name: String) -> String {
-    return VIDEO_PAGE_TEMPLATE.replace("{file_name}", &file_name);
-}
-
-
-fn list_files_to_html(path_to_dir: &str) -> String {
-    let mut result: String = String::new();
-    result.push_str("<html><body><ul>");
-    let dir = fs::read_dir(path_to_dir);
-    match dir {
-        Result::Err(_) => { result.push_str(format!("ERROR Reading dir: {}", path_to_dir).as_str()) }
-        Result::Ok(read_dir) => {
-            for entry in read_dir {
-                match entry {
-                    Ok(dir_entry) => {
-                        match dir_entry_to_html(dir_entry) {
-                            Ok(entry) => {
-                                result.push_str(entry.as_str());
-                            }
-                            Err(_) => {}
-                        }
-                    }
-                    Err(err) => eprintln!("Error: {}", err)
-                }
-            }
-        }
-    }
-    result.push_str("</ul></body></html>");
-    return result;
-}
-
-fn dir_entry_to_html(dir_entry: DirEntry) -> Result<String, std::io::Error> {
-    let file = dir_entry.file_name();
-    let meta = dir_entry.metadata()?;
-    return match file.to_str() {
-        Some(name) => {
-            if meta.is_file() {
-                return Ok(format!("<li><a href=\"/video/{}\">{}</a></li>", name, name));
-            }
-            if meta.is_dir() {
-                return Ok(format!("<li><a href=\"/dir/{}\">[ {} ]</a></li>", name, name));
-            }
-            Ok(String::new())
-        }
-        None => { Ok(String::new()) }
-    };
 }
--- src/stream/video.rs
+++ src/stream/video.rs
@@ -1,23 +1,64 @@
-use warp::Reply;
-use warp::hyper::Body;
-use warp::http::{Response, HeaderValue};
-use warp::hyper::header::CONTENT_TYPE;
+use std::cmp::min;
+use std::fs;
+use std::fs::File;
+use std::io::{Read, Seek, SeekFrom};
+use std::path::PathBuf;
 
-// todo real streaming no buffering?
+use warp::http::{HeaderValue, Response};
+use warp::hyper::Body;
+use warp::hyper::header::{CONTENT_RANGE, CONTENT_TYPE};
+use warp::Reply;
+
+use crate::stream::range::Range;
+
+// todo real streaming in the future
+// how it works usually with html5, browser asks for the whole file (even if its very big)
+// but reads the stream only slowly (ste by step to fill a buffer ahead)
+
+// todo discuss streams in sig
+
 pub struct VideoStream {
-    pub bytes: Vec<u8>
+    pub bytes: Vec<u8>,
+    pub start: u64,
+    pub end: u64,
+    pub overall_size: u64,
 }
 
 impl Reply for VideoStream {
     #[inline]
     fn into_response(self) -> Response<Body> {
         warp::http::Response::builder()
-            .header(
-                CONTENT_TYPE,
-                HeaderValue::from_static("application/octet-stream"),
+            .header(CONTENT_TYPE, HeaderValue::from_static("video/mp4"))
+            .header(CONTENT_RANGE, format!("bytes {}-{}/{}", self.start, self.end, self.overall_size),
             )
-            .status(200)
+            .status(if self.end - self.start == self.overall_size { 200 } else { 206 })
             .body(Body::from(self.bytes))
             .unwrap()
     }
+}
+
+pub fn read_file_range_to_video_stream(file_path: PathBuf, range: Range, max_size: u64) -> VideoStream {
+    let mut f = File::open(&file_path).expect("no file found");
+    let metadata = fs::metadata(&file_path).expect("unable to read metadata");
+    let chunk_size = calculate_chunk_size(&range, max_size, metadata.len());
+
+    let _res= f.seek(SeekFrom::Start(range.start));
+    let mut buffer = vec![0; chunk_size as usize];
+    f.read(&mut buffer).expect("todo error handling");
+
+    VideoStream {
+        bytes: buffer,
+        start: range.start,
+        end: range.start + chunk_size,
+        overall_size: metadata.len(),
+    }
+}
+
+// todo unit tests
+fn calculate_chunk_size(range: &Range, max_size: u64, file_length: u64) -> u64 {
+    let res = min(max_size, file_length - range.start);
+    return match range.end {
+        Some(end) => { min(end - range.start, res) }
+        None => res
+    };
 }
