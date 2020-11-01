f22d68bbdce840c9021d0980d90c22ec2fdfffe2
csviri <csviri@gmail.com>
2020-11-01 22:31:32 +0100

    start of error handling refactor

--- src/browse/dir.rs
+++ src/browse/dir.rs
@@ -5,41 +5,38 @@ use urlencoding::encode;
 
 static VIDEO_PAGE_TEMPLATE: &'static str = include_str!("video.html");
 
+#[derive(Debug)]
+enum BrowseError {
+    IoError(std::io::Error)
+}
+
+// from errpr
+
 pub fn video_html(encoded_file_path: String) -> String {
     return VIDEO_PAGE_TEMPLATE.replace("{file_name}", &encoded_file_path);
 }
 
-
-pub fn list_files_to_html(root_dir: &str, sub_dir_path: &str) -> String {
+pub fn list_files_to_html(root_dir: &str, sub_dir_path: &str) -> Result<String, BrowseError> {
     let mut result: String = String::new();
     result.push_str("<html><body><ul>");
     let mut path = PathBuf::from(root_dir);
     path.push(sub_dir_path);
 
-    let dir = fs::read_dir(path);
-    match dir {
-        Result::Err(_) => { result.push_str(format!("ERROR Reading dir: {}/{}", root_dir, sub_dir_path).as_str()) }
-        Result::Ok(read_dir) => {
-            for entry in read_dir {
-                match entry {
-                    Ok(dir_entry) => {
-                        match entry_to_html(dir_entry, sub_dir_path) {
-                            Ok(entry) => {
-                                result.push_str(entry.as_str());
-                            }
-                            Err(_) => {}
-                        }
-                    }
-                    Err(err) => eprintln!("Error: {}", err)
-                }
+    let dir = fs::read_dir(path)?;
+    for entry in dir {
+        match entry {
+            Ok(dir_entry) => {
+                let entry = entry_to_html(dir_entry, sub_dir_path)?;
+                result.push_str(entry.as_str());
             }
+            Err(err) => eprintln!("Error: {}", err)
         }
     }
     result.push_str("</ul></body></html>");
-    return result;
+    return Ok(result);
 }
 
-pub fn entry_to_html(dir_entry: DirEntry, sub_dir_path: &str) -> Result<String, std::io::Error> {
+fn entry_to_html(dir_entry: DirEntry, sub_dir_path: &str) -> Result<String, std::io::Error> {
     let file = dir_entry.file_name();
     let meta = dir_entry.metadata()?;
     return match file.to_str() {
